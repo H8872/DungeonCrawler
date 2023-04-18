54cca8125d1b5e3f70f8b692be11282e806acc26
H8872 <mm-laine@hotmail.fi>
2023-04-18 15:56:12 +0300

    add melee

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    [SerializeField] float hp, moveSpeed;
+    [SerializeField] float hp, rollCd, attackCd, moveSpeed;
     Animator anim;
     Rigidbody2D rb;
-    float moveForce;
-    Vector2 direction;
+    Transform attackPivot;
+    float moveForce, rollTimer, attackTimer, xinput, yinput;
+    Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = transform.GetComponent<Animator>();
         rb = transform.GetComponent<Rigidbody2D>();
-        
+        attackPivot = transform.GetChild(1);
+        direction = transform.right;
     }
 
     // Update is called once per frame
@@ -23,30 +25,48 @@ public class PlayerScript : MonoBehaviour
     {
         moveForce = moveSpeed * 1000;
 
-        if(Input.GetAxisRaw("Horizontal")<0)
+        xinput = Input.GetAxisRaw("Horizontal");
+        yinput = Input.GetAxisRaw("Vertical");
+
+        attackPivot.position = transform.position + direction/2;
+        attackPivot.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+
+        if(xinput<0)
             transform.GetComponent<SpriteRenderer>().flipX = true;
-        else if(Input.GetAxisRaw("Horizontal")>0)
+        else if(xinput>0)
             transform.GetComponent<SpriteRenderer>().flipX = false;
-        anim.SetFloat("Speed", rb.velocity.magnitude);
-        if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+
+        if(xinput == 0 && yinput == 0)
         {
-            anim.Play("Gobbo_Idle");
-            anim.SetFloat("Speed", 0);
+            anim.SetBool("Walkin", false);
         }
         else 
         {
-            anim.SetFloat("Speed", rb.velocity.magnitude);
-            direction = rb.velocity.normalized;
+            anim.SetBool("Walkin", true);
+            direction = new Vector3(xinput,yinput,0);
         }
-        if(Input.GetButtonDown("Jump"))
+        
+        if(rollTimer > 0) rollTimer -= Time.deltaTime;
+        if(Input.GetButtonDown("Jump") && rollTimer <= 0)
         {
             rb.AddForce(direction*(moveForce/2));
+            rollTimer = rollCd;
+        }
+
+        if(attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+        }
+        if(Input.GetButtonDown("Fire1") && attackTimer <= 0)
+        {
+            anim.Play("Gobbo_Attack");
+            attackTimer = attackCd;
         }
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal")*Time.deltaTime*moveForce,Input.GetAxisRaw("Vertical")*Time.deltaTime*moveForce));
+        rb.AddForce(new Vector2(xinput*Time.deltaTime*moveForce,yinput*Time.deltaTime*moveForce));
         
     }
 }
